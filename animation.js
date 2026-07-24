b4e34a32dd3a19106b83d7c32dfc0c7b0586f9db
vinayakmishraanp <mishravinayakanp@gmail.com>
2026-07-24 20:41:57 +0530

    Deleted some non usable files

--- animation.js
+++ animation.js
@@ -36,3 +36,22 @@ document.addEventListener("DOMContentLoaded", () => {
   window.addEventListener('scroll', scrollHandler);
   scrollHandler(); 
 });
+
+
+const slider = document.querySelector(".slides");
+
+let index = 0;
+
+const total = document.querySelectorAll(".slides video").length;
+
+setInterval(() => {
+
+    index++;
+
+    if(index >= total){
+        index = 0;
+    }
+
+    slider.style.transform = `translateX(-${index * 320}px)`;
+
+},3000);
